9c0b570b94430ddbf470f6c586e8217a8fbc9c31
daiki.omori <daiki0619@gmail.com>
2018-10-14 09:29:32 +0000

    top page

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,6 +1,6 @@
-Rails.application.config.gnerators do |g|
+Rails.application.config.generators do |g|
   g.stylesheets false
-  g.javascript false
+  g.javascripts false
   g.helper false
   g.skip_routes true
 end
